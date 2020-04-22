f937f1f7e74460ad435639350faef3029b832906
SoWeGoOn <SoWeGoOn>
2020-04-22 02:47:11 -0400

    Closer to server fixes - small pesky bug on retrieving the same name still avaliable. Also CSS/JS files in HTML point to the correct location

--- misc.py
+++ misc.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import os
 
+#MISC file for testing, ignore or delete
+
 DataReading = "data.csv"
 DataTraining = "training_data.csv"
 
-df = pd.read_csv(os.path.join(os.getcwd(),DataReading),encoding = "ISO-8859-1", engine="python")
+df1 = pd.read_csv(os.path.join(os.getcwd(),DataReading),encoding = "ISO-8859-1", engine="python", dtype=str, error_bad_lines=False)
+df2 = pd.read_csv(os.path.join(os.getcwd(),DataTraining), encoding = "ISO-8859-1", engine="python", error_bad_lines=False)
 
-value1 = df.loc[df['Name']=='Csbtkrillex'].index[0]
+for i in range(df1.shape[0]):
+    #Check to see if this user has been compared before, if its not, get their values
+    if df1.loc[i]['Name'] in df2['Name']:
+        print(df1.iloc[i,0:])
 
-value2 = df.iloc[value1, 0:].tolist()
-
-print(df.shape[0])
+print(df1.shape[0])
 print("------")
 #print(value2)
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import requests
 import numpy as np
 import pandas as pd
 import os
+import sys
+sys.dont_write_bytecode = True
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__)
 #Dont cache anything
@@ -18,18 +20,12 @@ def LoadAPI(currUser = None, botMark = None):
 
     #Read the current path
     if os.path.exists(os.path.join(os.getcwd(), DataReading)) == True:
-        df1 = pd.read_csv(os.path.join(os.getcwd(),DataReading),encoding = "ISO-8859-1", engine="python", dtype=str)
+        df1 = pd.read_csv(os.path.join(os.getcwd(),DataReading),encoding = "ISO-8859-1", engine="python", dtype=str, error_bad_lines=False)
     else:
         print("Specified Data Path Does Not Exist!")
         os._exit()
 
-    #Ready to write results to 
-    if os.path.exists(os.path.join(os.getcwd(), DataTraining)) == False:
-        print("Training data path does not exist, creating")
-        f = open(os.path.join(os.getcwd(), DataTraining), "w")
-        f.write("Name,data,is_bot")
-        f.close()
-    df2 = pd.read_csv(os.path.join(os.getcwd(),DataReading),encoding = "ISO-8859-1", engine="python")
+    df2 = pd.read_csv(os.path.join(os.getcwd(),DataTraining), encoding = "ISO-8859-1", engine="python", error_bad_lines=False)
 
 
     if not currUser:
@@ -37,16 +33,22 @@ def LoadAPI(currUser = None, botMark = None):
         for i in range(df1.shape[0]):
             #Check to see if this user has been compared before, if its not, get their values
             if df1.loc[i]['Name'] not in df2['Name']:
-               return ReturnToUser(df1.iloc[i,0:])
+                return ReturnToUser(df1.iloc[i,0:])
     else:
         #Write the status
-        writeData = open(os.path.join(os.getcwd(), DataReading), "a")
+        writeData = open(os.path.join(os.getcwd(), DataTraining), "a+")
+        print("Reading Location {} in df1".format(currUser))
         getLoc = df1.loc[df1['Name']==currUser].index[0]
+        print("Location of {} in df1 found at {}".format(currUser, getLoc))
+        print("Write to file")
+        writeData.write("\n")
         writeData.write("{},{},{}".format(currUser, df1.iloc[getLoc, 1:].tolist(), botMark))
+        #Reread the training data for the new info
+        df2 = pd.read_csv(os.path.join(os.getcwd(),DataTraining), encoding = "ISO-8859-1", engine="python", error_bad_lines=False)
         for i in range(df1.shape[0]):
             #Check to see if this user has been compared before, if its not, get their values
             if df1.loc[i]['Name'] not in df2['Name']:
-               return ReturnToUser(df1.iloc[i,0:])
+                return ReturnToUser(df1.iloc[i,0:])
 
 def ReturnToUser(info):
     valueFormat = info
@@ -93,6 +95,8 @@ def ReturnToUser(info):
     "location": valueFormat["Location"]
     }
 
+
+
 def FetchIcon(id):
     idCopy = int(id)
     if idCopy == -1:
@@ -117,5 +121,19 @@ def api():
 def root():
     return app.send_static_file("index.html")
 
+@app.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 if __name__ == '__main__':
+    if os.path.exists(os.path.join(os.getcwd(), DataTraining)) == False:
+        print("Training data {} does not exist, creating".format(DataTraining))
+        f = open(os.path.join(os.getcwd(), DataTraining), "w")
+        f.write("Name,data,is_bot")
+        f.close()
+    else:
+        print("Training data {} detected".format(DataTraining))
     app.run(port=5003, debug=True)
